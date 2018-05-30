9d61faf7742e37810c2c5cd84a5b1d439f24ef66
Ilan <ilan@dooble.co.il>
2018-05-30 21:00:27 +0300

    a little testing, something will work eventually

--- SOCKET-IO-SERVER/app.js
+++ SOCKET-IO-SERVER/app.js
@@ -22,10 +22,11 @@ io.on("connection", socket => {
 
 const getApiAndEmit = async socket => {
   try {
-    const res = await axios.get(
-      "https://api.darksky.net/forecast/PUT_YOUR_API_KEY_HERE/43.7695,11.2558"
-    );
-    socket.emit("FromAPI", res.data.currently.temperature);
+    socket.emit("shotExampleEvent", new Date());
+    // const res = await axios.get(
+    //   "https://api.darksky.net/forecast/PUT_YOUR_API_KEY_HERE/43.7695,11.2558"
+    // );
+    //socket.emit("FromAPI", res.data.currently.temperature);
   } catch (error) {
     console.error(`Error: ${error.code}`);
   }
